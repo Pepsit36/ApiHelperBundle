a366788bc989138ea0522854c3fd68818bb3abda
Sebastien Duplessy <sebastien@duplessy.eu>
2018-06-07 14:19:06 +0200

    Add controller helper

---
+++ Controller/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Pepsit36\ApiHelperBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ApiController extends Controller
+{
+	protected $serializer;
+
+	public function __construct(SerializerInterface $serializer)
+	{
+		$this->serializer = $serializer;
+	}
+
+	protected function createJsonResponse($data, array $groups = ['default']): JsonResponse
+	{
+		$responseData = $this->normalizeData($data, $groups);
+
+		return new JsonResponse(
+			$responseData
+		);
+	}
+
+	protected function normalizeData($data, array $groups = ['default']): array
+	{
+		return $this->serializer->normalize(
+			$data,
+			null,
+			['groups' => $groups]
+		);
+	}
+
+}
